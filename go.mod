b595da32bbc92fb0e52e52899398dac98b3aee60
nate <clevelandsun@gmail.com>
2021-07-19 23:48:07 -0400

    added rest client to query users

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/gocql/gocql v0.0.0-20210707082121-9a3953d1826d // indirect
+	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 )
---
+++ src/repository/restClient/users_repository.go
@@ -0,0 +1,53 @@
+package restClient
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/FreeCodeUserJack/bookstore_oauth/src/domain/users"
+	"github.com/FreeCodeUserJack/bookstore_oauth/src/utils/errors"
+	"github.com/mercadolibre/golang-restclient/rest"
+)
+
+var (
+	usersRestClient = rest.RequestBuilder{
+		BaseURL: "https://api.bookstore.com",
+		Timeout: 100*time.Millisecond,
+	}
+)
+
+type RestUserRepository interface {
+	LoginUser(string, string) (*users.User, *errors.RestError)
+}
+
+type usersRepository struct {
+}
+
+func NewRepository() RestUserRepository {
+	return &usersRepository{}
+}
+
+func (u *usersRepository) LoginUser(email, password string) (*users.User, *errors.RestError) {
+	request := users.UserLoginRequest{
+		Email: email,
+		Password: password,
+	}
+	response := usersRestClient.Post("/users/login", request)
+	if response == nil || response.Response == nil {
+		return nil, errors.NewInternalServerError("invalid restclient response when trying to login user", "internal server error")
+	}
+	if response.StatusCode > 299 {
+		var restErr errors.RestError
+		err := json.Unmarshal(response.Bytes(), &restErr)
+		if err != nil {
+			return nil, errors.NewInternalServerError("invalid error interface when trying to login user", "internal server error")
+		}
+		return nil, &restErr
+	}
+	var user users.User
+	if err := json.Unmarshal(response.Bytes(), &user); err != nil {
+		return nil, errors.NewInternalServerError("error trying to umarshal users response", "internal server error")
+	}
+
+	return &user, nil
+}
